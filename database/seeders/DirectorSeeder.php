e34083b8366217bb24c86d040b651762ec7648d3
cerberus2477 <3041TAN-12@tanuzem.boronkay.local>
2025-09-30 14:01:56 +0200

    Kész

--- database/seeders/DirectorSeeder.php
+++ database/seeders/DirectorSeeder.php
@@ -14,12 +14,26 @@ class DirectorSeeder extends Seeder
     public function run(): void
     {
         $directors = [
-            'Christopher Nolan',
-            'Steven Spielberg',
-            'Quentin Tarantino',
-            'Martin Scorsese',
-            'James Cameron',
-            'Peter Jackson'
+            'Nincs a listába',
+            'Christopher Nolan',        // Inception (DiCaprio), The Dark Knight Trilogy (Christian Bale)
+            'Steven Spielberg',         // Catch Me If You Can (DiCaprio, Tom Hanks)
+            'Quentin Tarantino',        // Once Upon a Time in Hollywood (DiCaprio, Brad Pitt)
+            'Martin Scorsese',          // The Wolf of Wall Street (DiCaprio)
+            'James Cameron',            // Titanic (DiCaprio, Kate Winslet), Avatar (Sam Worthington, Zoe Saldana)
+            'Peter Jackson',            // Lord of the Rings (Cate Blanchett), King Kong (Naomi Watts)
+            'Ridley Scott',             // Gladiator (Russell Crowe), The Martian (Matt Damon)
+            'David Fincher',            // Fight Club (Brad Pitt), The Social Network
+            'Guillermo del Toro',       // Crimson Peak (Jessica Chastain), Shape of Water
+            'Tim Burton',               // Alice in Wonderland (Johnny Depp, Anne Hathaway)
+            'Denis Villeneuve',         // Blade Runner 2049 (Ryan Gosling), Dune (Zendaya)
+            'Greta Gerwig',             // Little Women (Saoirse Ronan, Emma Watson)
+            'Clint Eastwood',           // Gran Torino, Million Dollar Baby (Hilary Swank)
+            'Robert Zemeckis',          // Forrest Gump (Tom Hanks), Cast Away (Tom Hanks)
+            'Francis Ford Coppola',     // The Godfather (Al Pacino, Robert De Niro)
+            'Sofia Coppola',            // Lost in Translation (Scarlett Johansson, Bill Murray)
+            'Ron Howard',               // A Beautiful Mind (Russell Crowe, Jennifer Connelly)
+            'Guy Ritchie',              // Sherlock Holmes (Robert Downey Jr.), The Gentlemen
+            'Patty Jenkins',            // Wonder Woman (Gal Gadot, Chris Pine)
         ];
 
         foreach ($directors as $dir) {
--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -49,6 +49,166 @@ class MovieSeeder extends Seeder
                 'category_id'=>1,
                 'actors'=>[3,4,6],
                 'cover_image'=>'films/film_4.jpg'
+            ],
+            [
+                'title'=>'Csillagok között',
+                'description'=>'Christopher Nolan grandiózus sci-fi drámája, amelyben a Föld haldokló környezete miatt az emberiség új otthont keres a csillagok között. Egy csapat űrhajós féreglyukon át utazik, hogy új lakható bolygót találjon, miközben az idő és a tér törvényei próbára teszik a hősök bátorságát és családi kötelékeiket.',
+                'director_id'=>1,
+                'category_id'=>1,
+                'actors'=>[1,6],
+                'cover_image'=>'films/film_5.jpg'
+            ],
+            [
+                'title'=>'Szárnyas fejvadász 2049',
+                'description'=>'Denis Villeneuve mesterműve, amely Ridley Scott klasszikusának folytatása. A történet egy új replikáns nyomozó, K életét követi, aki egy olyan titokra bukkan, amely megkérdőjelezi az egész társadalmi rendet és az emberi lét jövőjét.',
+                'director_id'=>8,
+                'category_id'=>1,
+                'actors'=>[1,3],
+                'cover_image'=>'films/film_6.jpg'
+            ],
+            [
+                'title'=>'Harcosok klubja',
+                'description'=>'David Fincher kultikus filmje, amely a modern fogyasztói társadalom kiüresedését boncolgatja. Egy kiégett irodista találkozik a karizmatikus Tyler Durdennel, és együtt titkos harcos klubot alapítanak, amely hamarosan globális anarchista mozgalommá válik.',
+                'director_id'=>9,
+                'category_id'=>7,
+                'actors'=>[2,5],
+                'cover_image'=>'films/film_7.jpg'
+            ],
+            [
+                'title'=>'Gladiátor',
+                'description'=>'Ridley Scott epikus történelmi filmje, amelyben Maximus, a római hadvezér, árulás áldozata lesz, és rabszolgasorba taszítják. A gladiátorviadalok során azonban új erőre kap, és bosszút esküszik a császár ellen.',
+                'director_id'=>10,
+                'category_id'=>4,
+                'actors'=>[5],
+                'cover_image'=>'films/film_8.jpg'
+            ],
+            [
+                'title'=>'Forrest Gump',
+                'description'=>'Tom Hanks felejthetetlen alakításában egy alacsony IQ-val rendelkező, de aranyszívű férfi történetét követjük, aki akaratlanul is részt vesz az amerikai történelem legfontosabb pillanataiban, miközben élete egyetlen szerelméért harcol.',
+                'director_id'=>11,
+                'category_id'=>5,
+                'actors'=>[5],
+                'cover_image'=>'films/film_9.jpg'
+            ],
+            [
+                'title'=>'A Keresztapa',
+                'description'=>'Francis Ford Coppola legendás filmje a Corleone maffiacsaládról, amely a hatalom, a lojalitás és a család közötti bonyolult kapcsolatokat vizsgálja. A film Michael Corleone felemelkedését mutatja be a család fejévé válásig.',
+                'director_id'=>12,
+                'category_id'=>7,
+                'actors'=>[2],
+                'cover_image'=>'films/film_10.jpg'
+            ],
+            [
+                'title'=>'Shutter Island',
+                'description'=>'Martin Scorsese pszichológiai thrillere, amelyben két nyomozó egy elmegyógyintézet lakóinak rejtélyes eltűnését vizsgálja. Ahogy egyre mélyebbre ásnak, lassan kiderül, hogy semmi sem az, aminek látszik.',
+                'director_id'=>4,
+                'category_id'=>7,
+                'actors'=>[1],
+                'cover_image'=>'films/film_11.jpg'
+            ],
+            [
+                'title'=>'A sötét lovag',
+                'description'=>'Christopher Nolan Batman-trilógiájának második része, amelyben a denevérember szembekerül a kaotikus Jokerrel. A film sötét tónusával és Heath Ledger legendás alakításával új szintre emelte a képregényfilmeket.',
+                'director_id'=>1,
+                'category_id'=>2,
+                'actors'=>[1,2,3],
+                'cover_image'=>'films/film_12.jpg'
+            ],
+            [
+                'title'=>'A remény rabjai',
+                'description'=>'Frank Darabont megható drámája, amely Stephen King regényén alapul. Andy Dufresne, egy ártatlanul elítélt bankár barátságot köt egy másik rabbal, Red-del, és reménye tartja életben a börtön kegyetlen világában.',
+                'director_id'=>13,
+                'category_id'=>5,
+                'actors'=>[5],
+                'cover_image'=>'films/film_13.jpg'
+            ],
+            [
+                'title'=>'Az ötödik elem',
+                'description'=>'Luc Besson vizuálisan lenyűgöző sci-fije, amelyben egy taxisofőr és egy titokzatos nő próbálja megakadályozni, hogy az univerzumot elpusztítsa a gonosz. A film színes látványvilága és különc karakterei igazi kultklasszikussá tették.',
+                'director_id'=>14,
+                'category_id'=>1,
+                'actors'=>[6],
+                'cover_image'=>'films/film_14.jpg'
+            ],
+            [
+                'title'=>'Amerikai szépség',
+                'description'=>'Sam Mendes Oscar-díjas filmje, amely egy kertvárosi család életén keresztül mutatja be a modern amerikai álom széthullását. A film egyszerre kritikus és szatirikus képet fest a boldogság hajszolásáról.',
+                'director_id'=>15,
+                'category_id'=>5,
+                'actors'=>[2,6],
+                'cover_image'=>'films/film_15.jpg'
+            ],
+            [
+                'title'=>'Szellemirtók',
+                'description'=>'Ivan Reitman vígjátéka, amelyben egy különc tudósokból álló csapat kísértetirtó vállalkozást indít New Yorkban. A film humorával és látványos trükkjeivel vált generációk kedvencévé.',
+                'director_id'=>16,
+                'category_id'=>6,
+                'actors'=>[5],
+                'cover_image'=>'films/film_16.jpg'
+            ],
+            [
+                'title'=>'A Mátrix',
+                'description'=>'A Wachowski testvérek forradalmi sci-fije, amely bemutatja, hogy a világ, amelyben élünk, valójában egy szimuláció. Neo, a számítógépes hacker, ráébred valódi szerepére, mint az emberiség megmentője.',
+                'director_id'=>17,
+                'category_id'=>1,
+                'actors'=>[2,3],
+                'cover_image'=>'films/film_17.jpg'
+            ],
+            [
+                'title'=>'Inception',
+                'description'=>'Egy különleges akcióthriller, amely az álmok rétegeibe kalauzol minket. A csapat a tudat mélyébe hatol, hogy végrehajtson egy komplex "beültetési" küldetést, miközben minden lépés veszélyt hordoz a valóságra és személyes életükre is.',
+                'director_id'=>1,
+                'category_id'=>1,
+                'actors'=>[1,2,3],
+                'cover_image'=>'films/film_18.jpg'
+            ],
+            [
+                'title'=>'Jurassic Park',
+                'description'=>'Steven Spielberg klasszikusa, ahol egy dinoszauruszpark technológiai csodái váratlanul elszabadulnak. A tudósok, gyermekek és látogatók küzdenek a túlélésért egy lenyűgöző, de veszélyes világban.',
+                'director_id'=>2,
+                'category_id'=>4,
+                'actors'=>[4,5],
+                'cover_image'=>'films/film_19.jpg'
+            ],
+            [
+                'title'=>'Interstellar',
+                'description'=>'Christopher Nolan grandiózus sci-fi filmje az emberiség jövőjéről és a szeretet erejéről, ami átível az időn és téridőn. A csapat féreglyukakon keresztül kutat új lakható bolygók után.',
+                'director_id'=>1,
+                'category_id'=>1,
+                'actors'=>[1,6],
+                'cover_image'=>'films/film_20.jpg'
+            ],
+            [
+                'title'=>'Dűne',
+                'description'=>'Denis Villeneuve monumentális sci-fi adaptációja Frank Herbert regényéből. Paul Atreides és családja az Arrakis bolygón találja magát, ahol politika, hatalom és sivatagi titkok keverednek egy epikus kalandban.',
+                'director_id'=>8,
+                'category_id'=>1,
+                'actors'=>[2,3,6],
+                'cover_image'=>'films/film_21.jpg'
+            ],
+            [
+                'title'=>'Tenet',
+                'description'=>'Christopher Nolan időmanipulációs thrillere, ahol a főhős a jövő és múlt összefonódó eseményeit próbálja megakadályozni, hogy globális katasztrófa következzen be. A film látványos akcióval és bonyolult időlogikával operál.',
+                'director_id'=>1,
+                'category_id'=>1,
+                'actors'=>[1,2,3,6],
+                'cover_image'=>'films/film_22.jpg'
+            ],
+            [
+                'title'=>'Blade Runner',
+                'description'=>'Ridley Scott kultikus sci-fi filmje, ahol a replikánsok és emberek közötti határ elmosódik. A nyomozó, Rick Deckard, az emberi létezés és a gépi tudat kérdéseit vizsgálja egy sötét, neonfényes jövőben.',
+                'director_id'=>10,
+                'category_id'=>1,
+                'actors'=>[3,5],
+                'cover_image'=>'films/film_23.jpg'
+            ],
+            [
+                'title'=>'Avatar',
+                'description'=>'James Cameron vizuális csodája, amely a Pandora bolygón játszódik. Az emberek és a Na’vi őslakosok konfliktusa során a főhős új perspektívát talál a szeretetre, hűségre és a természet tiszteletére.',
+                'director_id'=>6,
+                'category_id'=>1,
+                'actors'=>[1,6],
+                'cover_image'=>'films/film_24.jpg'
             ]
         ];
 
